daf750919193ea7ad62ffbca00a6ae5bdca9fe38
aarik <roblesaarik@gmail.com>
2020-06-28 14:38:29 -0700

    Sound update

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 240f;
     [SerializeField] float mainThrust = 5f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip dyingSound;
+    [SerializeField] AudioClip finishLevel;
+    
+
     
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +29,32 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        Thruster();
-        Rotate();
+        if (state == State.Alive) {
+         RespondToThrustInput();
+         RespondToRotate();
+        }
     }
 
-    void Thruster() {
+    void RespondToThrustInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if(!audioSource.isPlaying) {
-
-            audioSource.Play();
+            
+            ApplyThruster();
 
             } else {
                 audioSource.Stop();
             }
         }
+    
+
+    void ApplyThruster() {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if(!audioSource.isPlaying) {
+
+            audioSource.PlayOneShot(mainEngine);
+    }
     }
 
-    void Rotate() {
+    void RespondToRotate() {
 
         rigidBody.freezeRotation = true;  // Manual control of rotation
         
@@ -57,19 +74,44 @@ public class Rocket : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
+
+        if (state != State.Alive ) { return; } // Ignores collisions when dead
+
         switch (collision.gameObject.tag) {
             case "Friendly":
                 print("OK");
             break;
-            
-            case "Fuel":
-                print("Fuel");
+
+            case "Finish":
+                Success();
             break;
 
             default: 
-                print("Dead");
+                Death();
             break;
         }
+    }
+
+    void Success() {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishLevel);
+        Invoke("LoadNextScene", 1f);
+    }
+
+    void Death() {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(dyingSound);
+        Invoke("LoadFirstScene", 1f); 
+    }
+
+     void LoadNextScene() {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadFirstScene() {
+        SceneManager.LoadScene(0);
     }
     
 }
